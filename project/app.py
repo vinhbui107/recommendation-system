22518a71f565636a9eaa92984c68425a501ef57b
vinhbui107 <vinhbui.tg@gmail.com>
2021-04-24 14:37:46 +0700

    add api for model

---
+++ project/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import psycopg2
+
+from utils import connect_db
+
+
+app = Flask(__name__)
+
+
+# CORS implemented so that we don't get errors when trying to access the server from a different server location
+CORS(app)
+
+
+# Display hello
+@app.route("/")
+def home():
+    return "Hello from Recommendation System api"
+
+
+# get movies recommend for user with username
+@app.route("/movies/recommend/<username>")
+def get_movies_recommend(username):
+    # Connect DB
+    cur = connect_db()
+    if not cur:
+        return "Connect Database failed"
+
+    return "It works"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
