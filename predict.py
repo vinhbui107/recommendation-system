d13fd79678092b8ec81dc88e467189ed49989492
vinhbui107 <vinhbui.tg@gmail.com>
2021-04-28 23:17:12 +0700

    create file for predict process

---
+++ predict.py
@@ -0,0 +1,19 @@
+import os
+
+from model import collaborative_filtering, demographic_filtering
+from utils.export_data import export_data
+from utils.helpers import get_ratings_data, get_users_data
+
+
+# set python path
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# export data
+
+# get data
+
+# predict data cf and save file
+
+# predcit data df and save file
+
+# done
